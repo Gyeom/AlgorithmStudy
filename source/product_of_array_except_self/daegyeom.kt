a80068c406a6a78d45ca1ce7be45e5376ca0c3fb
= <=>
2021-12-23 17:09:06 +0900

    add 4,5 step problems

---
+++ source/product_of_array_except_self/daegyeom.kt
@@ -0,0 +1,16 @@
+package product_of_array_except_self
+
+fun productExceptSelf(nums: IntArray): IntArray {
+    val output = IntArray(nums.size) { 1 }
+    var assistant = 1
+    for (index in nums.indices) {
+        output[index] *= assistant
+        assistant *= nums[index]
+    }
+    assistant = 1
+    for (index in nums.size - 1 downTo 0) {
+        output[index] *= assistant
+        assistant *= nums[index]
+    }
+    return output
+}
